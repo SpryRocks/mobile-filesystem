0e2649d9bd397ebd5a01340a2ea1e97844e2034d
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-12-13 00:48:49 +0100

    Refactoring

--- packages/core/src/IDirectory.ts
+++ packages/core/src/IDirectory.ts
@@ -2,5 +2,6 @@ import {IEntry} from './IEntry';
 import {IFile} from './IFile';
 
 export abstract class IDirectory extends IEntry {
-  abstract getFile(): Promise<IFile>;
+  abstract getFile(path: string): Promise<IFile>;
+  abstract getDirectory(path: string): Promise<IDirectory>;
 }
