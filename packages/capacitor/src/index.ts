9fa519cfc5d4d627f9cc9058d799e2a19a928589
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-12-12 23:04:27 +0100

    Add capacitor files

---
+++ packages/capacitor/src/index.ts
@@ -0,0 +1,8 @@
+import {IFileSystem} from '@spryrocks/mobile-filesystem-plugin-core';
+import {SystemDirectoryType} from './SystemDirectoryType';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const fileSystem: IFileSystem<SystemDirectoryType>;
+
+export {fileSystem as FileSystem};
