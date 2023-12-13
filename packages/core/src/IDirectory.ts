10c29a21901f9d9d7c57b4f848b0849246e246c6
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-12-13 01:02:29 +0100

    Refactoring

--- packages/core/src/IDirectory.ts
+++ packages/core/src/IDirectory.ts
@@ -1,7 +1,12 @@
 import {IEntry} from './IEntry';
 import {IFile} from './IFile';
 
+export type DeleteOptions = {
+  recursively?: boolean;
+};
+
 export abstract class IDirectory extends IEntry {
   abstract getFile(path: string): IFile;
   abstract getDirectory(path: string): IDirectory;
+  abstract delete(options?: DeleteOptions): Promise<void>;
 }
